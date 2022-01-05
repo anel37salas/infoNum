6336bf45b867505bb8e4fd9d54e104425bc6ac08
DW Dariel <84371541+DarkLeyh@users.noreply.github.com>
2022-01-04 22:57:27 -0700

    Update infoNum.py

--- infoNum.py
+++ infoNum.py
@@ -1,6 +1,7 @@
 #Tool created by DW Dariel(Gustavo Hernandez)
 #Tool created to study python libraries
 import requests
+from getpass import _raw_input
 from colorama import Fore, init
 init()
 
@@ -8,7 +9,7 @@ api = '510f8c00e437e5c107b8629d2fe5ca20'
 
 print(Fore.GREEN+"\nScript hecho por DW Dariel")
 
-numero = int(input("\nIngrese el numero: "))
+numero = int(_raw_input("\nIngrese el numero: "))
 
 info = requests.get("http://apilayer.net/api/validate?access_key=%s&number=%s&country_code&format=1" % (api, numero))
 
